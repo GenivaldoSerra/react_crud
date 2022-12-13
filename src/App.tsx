360b352372e3be65826d760d1452603aba217e18
GenivaldoSerra <>
2022-12-13 08:15:07 -0300

    feat: Instalando as dependencias e configurando as rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes/index';
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!!!!</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
